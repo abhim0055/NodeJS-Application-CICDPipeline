2d7eeebade1009fa5b33137531a2f653a68e7223
abhi menasinakai <abhim0055@gmail.com>
2024-03-03 09:04:37 +0530

    Updated app.js with new features

---
+++ app.js
@@ -0,0 +1,21 @@
+// Importing the built-in HTTP module
+const http = require('http');
+
+// Defining the hostname and port for the server
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// Creating a server instance using the HTTP module
+const server = http.createServer((req, res) => {
+  // Setting the response HTTP header
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  
+  // Sending the "Hello, World!" message to the client
+  res.end('Hello, World!\n');
+});
+
+// Starting the server and listening to incoming requests
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
